922cf7d5e6f26eeb13e9647523cdcf01fc832696
iwan <iwan@localhost.localdomain>
2017-10-25 22:38:49 +0200

    Add call to Client.connect() from main
    To show myself the difference between a unit-test that asserts
    a limited set of expected behavior with mocked dependencies
    and the main program with call to the real dependency, which is
    a thrid party library.
    
    
    Signed-off-by: iwan <iwan@localhost.localdomain>

--- src/main/java/com/flameling/mindef/telnet/Main.java
+++ src/main/java/com/flameling/mindef/telnet/Main.java
@@ -1,9 +1,13 @@
 package com.flameling.mindef.telnet;
 
+import org.apache.commons.net.telnet.TelnetClient;
+
 public class Main {
 
 	public static void main(String[] args){
 		System.out.println("Hello World!");
+		Client client = new Client(new TelnetClient());
+		client.connect();
 	}
 
 }
